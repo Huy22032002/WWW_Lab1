e2e29fb9dc05d5996e9a1dfd0161ff91f3120fda
Huy22032002 <ndhuy2203@gmail.com>
2024-09-16 21:35:17 +0700

    lan1

---
+++ src/main/java/fit/iuh/edu/vn/week01_lab_nguyenduchuy_20105761/entities/GrantAccess.java
@@ -0,0 +1,4 @@
+package fit.iuh.edu.vn.week01_lab_nguyenduchuy_20105761.entities;
+
+public class GrantAccess {
+}
---
+++ src/main/java/fit/iuh/edu/vn/week01_lab_nguyenduchuy_20105761/services/RoleService.java
@@ -0,0 +1,4 @@
+package fit.iuh.edu.vn.week01_lab_nguyenduchuy_20105761.services;
+
+public class RoleService {
+}
